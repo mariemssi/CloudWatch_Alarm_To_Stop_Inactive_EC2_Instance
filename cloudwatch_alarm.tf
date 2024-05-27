05614c873c5bd071b0689fee4590eec928cf20dc
marmar <meriemiag@yahoo.fr>
2024-05-27 03:41:46 +0200

    update comments

--- cloudwatch_alarm.tf
+++ cloudwatch_alarm.tf
@@ -1,6 +1,6 @@
 # Create a cloudwatch alarm for EC2 instances and alarm_actions to SNS topic and stop instance
 
-resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
+resource "aws_cloudwatch_metric_alarm" "ec2_cpu_alarm" {
   comparison_operator       = "LessThanOrEqualToThreshold"
   evaluation_periods        = "12"
   datapoints_to_alarm       = "12"
@@ -9,13 +9,13 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
   period                    = "300" #seconds
   statistic                 = "Average"
   threshold                 = "2"
-  alarm_description         = "This alarm stop the ec2 if it is inactive for 1 hour"
+  alarm_description         = "This alarm stops the EC2 instance if it remains inactive for one hour."
   treat_missing_data        = "missing"
   insufficient_data_actions = []
   alarm_actions             = [aws_sns_topic.topic.arn, "arn:aws:automate:${var.region}:ec2:stop"]
 
   alarm_name = "stop_test_instance_alarm"
-  
+
   dimensions = {
     InstanceId = var.instance_ID
   }
